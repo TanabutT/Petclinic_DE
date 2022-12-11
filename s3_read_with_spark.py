80127b88b0030a2765e807d2ff8a0f566f0e3663
TanabutT <t_tanabut@hotmail.com>
2022-12-12 00:53:02 +0700

    rm aws key after leak

--- s3_read_with_spark.py
+++ s3_read_with_spark.py
@@ -1,10 +1,9 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-AWS_ACCESS_KEY_ID = "ASIAWFHJWX4CYHJBFV65"
-AWS_SECRET_ACCESS_KEY = "kENd8eM8UTRTGML39PwYLrqWo/d/RUll9AIgCDBE"
-AWS_SESSION_TOKEN = "FwoGZXIvYXdzEHAaDJqV/FpGSwhsqoZKoSLHAUoWbX1KrWnzSJkvrZ4nbi0+bpk4Nlk3FIaK8yC+jALna10Fwxp6qyd75fJk9J6EzsGNGfMVxeHTJrpz91sgUGXNSnEeI5SRUmMjiJ2ln9AjHhuq/TKIbv73eqjdXS9Mw81mQFZ6OVHBCBSnOlve3MLyZNG5Qb+BRZXkbq9qHxaP/1g2gvGqWnvCrhxZVo+vHmTuDA/YXWAYb+mAv2QSGZfWCFteNt6gukBTwxHvhqLgR7EIHsYDBILRWYPh+tuvhMj/94Igq+Qo1v3VnAYyLY0BwfqyngQPrQ44EvE0rFOw8D8EYctOFdsWLcTEq2L0UNDJJWa3V/hKglnG1w=="
-
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+AWS_SESSION_TOKEN = ""
 
 conf = SparkConf()
 conf.set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.2")
