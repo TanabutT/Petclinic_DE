26cbc4406a84dc8e02f4105074bdc06310cd3137
TanabutT <t_tanabut@hotmail.com>
2022-12-17 15:29:14 +0000

    completed run elt csv and parquet

--- dags/elt_csv.py
+++ dags/elt_csv.py
@@ -1,12 +1,8 @@
 from typing import List
 import os
 import glob
-import json
-from os import getenv
-# from sql_queries import *
 from _get_files import _get_files
 from _con_upload_to_s3 import _con_upload_to_s3
-# from _s3_transform_with_spark import _s3_transform_with_spark
 from _create_Redshift import _create_Redshift
 from _create_tables_process import _create_tables_process
 from _read_bucket_landing_transform_to_cleaned_parquet import _read_bucket_landing_transform_to_cleaned_parquet
@@ -21,9 +17,6 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.amazon.aws.sensors.redshift_cluster import RedshiftClusterSensor
-
-
-# REDSHIFT_CLUSTER_IDENTIFIER = getenv("REDSHIFT_CLUSTER_IDENTIFIER", "redshift-cluster-1")
 
 with DAG (
     "elt_csv",
@@ -41,11 +34,11 @@ with DAG (
         }
     )
 
-    # create_Redshift = PythonOperator(
-    #     task_id="create_Redshift",
-    #     python_callable=_create_Redshift,
+    create_Redshift = PythonOperator(
+        task_id="create_Redshift",
+        python_callable=_create_Redshift,
         
-    # )
+    )
   
     con_upload_to_s3 = PythonOperator(
         task_id="con_upload_to_s3",
@@ -56,17 +49,6 @@ with DAG (
         task_id="read_bucket_landing_transform_to_cleaned_parquet",
         python_callable=_read_bucket_landing_transform_to_cleaned_parquet,
     )
-
-    # s3_transform_with_spark = PythonOperator(
-    #     task_id="s3_transform_with_spark",
-    #     python_callable=_s3_transform_with_spark,
-    #     op_kwargs={
-    #         "filepath" : "/opt/airflow/dags/data",
-    #     }
-    # )
-    # ti = context["ti"]
-    # # Get list of files from filepath
-    # ClusterIdentifier = ti.xcom_pull(task_ids="create_Redshift", key="return_value")
 
     describe_cluster = PythonOperator(
         task_id="describe_cluster",
@@ -92,11 +74,6 @@ with DAG (
        
     )
 
-    # insert_data_parquet = PythonOperator(
-    #     task_id="insert_data_parquet",
-    #     python_callable=_insert_data_parquet,
-       
-    # )
       
     get_files >> con_upload_to_s3 >> read_transform_parquet >> wait_for_cluster >> describe_cluster >> create_tables_process >> insert_data
-    # create_Redshift >> wait_for_cluster
+    create_Redshift >> wait_for_cluster
