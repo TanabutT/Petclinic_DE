0ed362a198593afb57d875446babd0168c430c9c
TanabutT <t_tanabut@hotmail.com>
2022-12-17 16:24:57 +0000

    test dbt

---
+++ dags/__pycache__/setting_dag.py
@@ -0,0 +1,10 @@
+aws_access_key_id = "ASIAWFHJWX4CW6XR65PR"
+aws_secret_access_key = "O4h3rmQHcgm895FUYyLu7/rnHHEnAFne1dy1ZNVi"
+aws_session_token = "FwoGZXIvYXdzEAUaDDr6ac4yeYIHBJakryLHAUkfRMO4nWuJ/AXKyJObQ37f5Gab82P2NalfxJXu64AntgPyCGhBoL62y+ZANFvFl7g7gdvAYAYETd/Bzovu+XOS933Jq8RpUQaVLRDABKUuw/6qb/L91sBEHqU4w+Fq6LqWq3Z/v1FEjQ9ajz8FQZij097++ACf93USQGdSGPmOfe1rBcEGGU/z9Rb0g703gmdqqUW415kze4FYPj9HMQYhCy2lNJkO3jrMHTDqV+ODMFpMZEFAuBSxeRdVGDEG0sEwD13vi1wo9932nAYyLa+6ATxi9C+Zy8hwt49jRjGAq36RejtYRkqT5caf/eLHnL7nf3Yvsjq1E5hf3w=="
+
+
+
+
+#Redshift Cluster "can not create" LabRole unable to access Redshift via boto3
+cluster_user = "awsuser"
+cluster_password = "eazhiKb228WYGZs"
